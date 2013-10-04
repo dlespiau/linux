a0f160629bc88a90e78979d949a465c62fb53b5c
Damien Lespiau <damien.lespiau@intel.com>
2013-10-04 15:08:44 +0100

    [squash]: Remove spurious addition of a blank line

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1393,7 +1393,6 @@ typedef struct drm_i915_private {
 	/* Old dri1 support infrastructure, beware the dragons ya fools entering
 	 * here! */
 	struct i915_dri1_state dri1;
-
 	/* Old ums support infrastructure, same warning applies. */
 	struct i915_ums_state ums;
 
